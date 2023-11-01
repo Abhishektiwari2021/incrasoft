1ca9b3eaf167fe802502e0721c4ee02832cfe764
harshasurya02 <harshasurya02@gmail.com>
2023-11-01 22:30:38 +0530

    Layout setup

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  showDropDown(): void {
+    let dropDownElement = document.getElementById('dropDownMenu');
+    dropDownElement?.classList.toggle('show');
+  }
 }
